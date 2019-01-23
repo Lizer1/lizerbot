77332d66c8cb4bd67523257a6ddd8786ec7577a8
Lizer1 <46378036+Lizer1@users.noreply.github.com>
2019-01-23 23:39:48 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -227,7 +227,7 @@ client.on('ready', () => {
       console.log(`ON ${client.guilds.size} Servers '     Script By : i1Suhaib ' `);
     console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`عمك ليزر العين`,"http://twitch.tv/S-F")
+client.user.setGame(`عمك ليزر العينc`,"http://twitch.tv/S-F")
 client.user.setStatus("dnd")
 });
 
