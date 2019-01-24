269cc13894187daf80996d0be285c595e7e22592
Lizer1 <46378036+Lizer1@users.noreply.github.com>
2019-01-24 22:55:10 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -219,16 +219,35 @@ function play(guild, song) {
 
 
 
-
+const Discord = require('discord.js');
+const client = new Discord.Client();
 client.on('ready', () => {
-   console.log(`----------------`);
-      console.log(`Desert Bot- Script By : i1Suhaib`);
-        console.log(`----------------`);
-      console.log(`ON ${client.guilds.size} Servers '     Script By : i1Suhaib ' `);
-    console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`عمك ليزر العينc`,"http://twitch.tv/S-F")
-client.user.setStatus("dnd")
+});
+
+
+const developers = ["532613290795335700"]
+const adminprefix = "1";
+client.on('message', message => {
+    var argresult = message.content.split(` `).slice(1).join(' ');
+      if (!developers.includes(message.author.id)) return;
+      
+  if (message.content.startsWith(adminprefix + 'p')) {
+    client.user.setGame(argresult);
+      message.channel.send(`**Status You   ${argresult}**`)
+  } else 
+  if (message.content.startsWith(adminprefix + 'w')) {
+  client.user.setActivity(argresult, {type:'WATCHING'});
+      message.channel.send(`**Status You   ${argresult}**`)
+  } else 
+  if (message.content.startsWith(adminprefix + 'l')) {
+  client.user.setActivity(argresult , {type:'LISTENING'});
+      message.channel.send(`**Status You  ${argresult}**`)
+  } else 
+  if (message.content.startsWith(adminprefix + 's')) {
+    client.user.setGame(argresult, "https://www.twitch.tv/M3roof");
+      message.channel.send(`**Status You ${argresult} **`)
+}
 });
 
 
